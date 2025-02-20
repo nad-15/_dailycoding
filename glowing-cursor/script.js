a457a9a3e617c91f3f1fa96974a6e9be03c3ae04
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-02-20 23:49:27 +0800

    Update script.js

--- glowing-cursor/script.js
+++ glowing-cursor/script.js
@@ -1,12 +1,16 @@
+let cursor = document.querySelector(`.glowing`);
+
 document.addEventListener(`mousemove`, (event) =>{
+    cursor.style.display = `block`;
     let mouseX = event.clientX -20 ;
     let mouseY = event.clientY - 20;
-
-    let cursor = document.querySelector(`.glowing`);
-
     cursor.style.left = mouseX + `px`;
     cursor.style.top = mouseY + `px`;
 
-    
 
+
+});
+
+document.addEventListener(`mouseleave`, ()=>{
+    cursor.style.display = `none`;
 });
